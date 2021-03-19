2c0daf06a5b599cd57177f20ea978f323757ad66
sectur1 <sethsamuel03@gmail.com>
2021-03-19 15:31:15 +0100

    master

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import {queries, types} from "github-apiv4"
 import fetch from "isomorphic-fetch";
 
-const fields = "fullDescription id name shortDescription"
 const accessCode = 'e589bd4b4ae70f25d2e8ca0c3ca0f1f4452a20a0'; 
-
-const query = queries.SponsorsListing("",fields)
+const user = queries.User("bio")
+const onUser = queries.onUser(user)
+const fields = `
+  ... on App { id }
+  ... on Organization {id description url}
+` 
+const query = queries.Search("facebook",10,fields,"USER")
 
 fetch('https://api.github.com/graphql', {
   method: 'POST',
@@ -15,14 +19,12 @@ fetch('https://api.github.com/graphql', {
   body: JSON.stringify({ query: query}),
 }).then(res => res.json().then(res => {
     if(res.data){
-      const resSponsorsListing:types.SponsorsListing = res.data
-      console.log(resSponsorsListing.sponsorsListing )
+      const resSponsorsListing:types.Search = res.data
+      console.log(resSponsorsListing.search.nodes )
     
     }else{
       console.log("An error occurred",res) 
-      console.log("Error data: ",res.data)
-      console.log(query )
-
+      console.log(query)
     }
   }))
   .catch(err => console.log(err));
